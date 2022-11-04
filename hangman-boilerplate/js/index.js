b73c65128be20e67cdc0f16eafece0a8aec151b0
Josefine Forsman <josefineforsman1@gmail.com>
2022-11-04 14:28:11 +0100

    layout design

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -1,5 +1,6 @@
-// array med ord att gissa på
+// array with the wordlist for the game.
 const wordList = ['mobil', 'päron', 'gurka', 'dator','tröja','byxor'];
+
 const ul = document.querySelector('ul');
 const ShowParts = document.querySelector("figure")
 let resetbtn = document.querySelector("#reset")
@@ -14,52 +15,52 @@ const LoseText = document.querySelector(".lose-text");
 const WinText = document.querySelector(".win-text");
 let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"]
 
-// Funktion som startar om sidan.
+// Function that reloads the page when "play again" btn is clicked.
 function reset(){
   location.reload();
 }
-// funktion som gör att det kommer upp en "game over-skärm".
+// Function that makes the game-over screen slide in.
 function toggleSliderLose() {
   slider.classList.toggle("show");
 }
 
-// funktion som gör att det kommer upp en "game over-skärm".
+//  Function that makes the win screen slide in.
 function toggleSliderWin() {
   slider1.classList.toggle("show");
 }
-// Tar tillbaka slider till rätt z-index igen.
+//When the play again button is pressed, the slider moves back to the correct index again.
 playAgainBtn.addEventListener("click", () => {
 toggleSliderLose();
 });
 
-// Tar tillbaka slider till rätt z-index igen.
+//When the play again button is pressed, the slider moves back to the correct index again.
 playAgainBtn2.addEventListener("click",() => {
   toggleSliderWin();
 } )
 
-//Slumpar random tal som görs om till en variabel.
+//Randomize a random number that is declared in a variable.
 let pickAnumber = Math.floor(Math.random() * wordList.length);
-
 const randomWord = wordList[pickAnumber];
+
+// An empty array that is filled with wrong letter guesses.
 const wrongGuesses = [];
 
-// Strecken som syns vid start, står för antalet bokstäver.
+// The lines that are visible at the start represent the number of letters.
 let rightLetter = [`<li>-</li>`,`<li>-</li>`,`<li>-</li>`,`<li>-</li>`,`<li>-</li>`,];
 
-
-// splittrar ordet till en array med bokstäver för att enklare kunna jämföra.
+//splits the word into an array of letters for easier comparison.
 const letters = randomWord.split('');
 
-// Vid knapptryck, jämförs userInput med olika villkor.
+
+// On button press, userInput is compared with various conditions.
 document.querySelector('body').addEventListener('keypress', (event) => {
+  
   
   const userInput = event.key;
   for (let i = 0; i < alphabet.length; i++) { 
     if (userInput == alphabet[i]) {
       
       let correctGuess = false;
-      console.log(event.key);
-      
       
       for(let i = 0; i<letters.length; i++) {
         if (userInput === letters[i]){
@@ -67,24 +68,19 @@ document.querySelector('body').addEventListener('keypress', (event) => {
 
           correctGuess = true;
           
-          console.log(rightLetter);
-          console.log('Rätt bokstav');
-          
           ul.textContent = '';
           rightLetter[i]  = `${letters[i]}`;
           ul.insertAdjacentHTML ('beforeend', rightLetter.join(""));
-          console.log(rightLetter.join(""));
         }
+
         if(randomWord == rightLetter.join("")){
           WinText.style.color = "white";
           WinText.style.backgroundColor = "green";
-          WinText.textContent= `DU VANN!`;
+          WinText.textContent= `YOU WON!!!`;
           toggleSliderWin();
-        }
-        
+        }  
       }
-
-    // När användaren gissar fel
+    // When the user make a wrong guess.
       if (correctGuess === false){
         console.log('Fel gissning');
         wrongGuesses.push(userInput);
@@ -93,28 +89,24 @@ document.querySelector('body').addEventListener('keypress', (event) => {
         
         if (wrongGuesses.length == 1){
           ShowParts.classList.add("scaffold")
-          showMaxGuesses.textContent = `You have ${maxGuesses-1} guesses left`;
-          
+          showMaxGuesses.textContent = `You have ${maxGuesses-1} guesses left`; 
         }
         else if (wrongGuesses.length == 2){
           ShowParts.classList.add("head")
-          showMaxGuesses.textContent = `You have ${maxGuesses-2} guesses left`;
-          
+          showMaxGuesses.textContent = `You have ${maxGuesses-2} guesses left`; 
         } 
         else if (wrongGuesses.length == 3){
           ShowParts.classList.add("body")
-          showMaxGuesses.textContent = `You have ${maxGuesses-3} guesses left`;
-          
+          showMaxGuesses.textContent = `You have ${maxGuesses-3} guesses left`; 
         }
         else if (wrongGuesses.length == 4){
           ShowParts.classList.add("arms")
           showMaxGuesses.textContent = `You have ${maxGuesses-4} guesses left`;
-          
         }
         else if (wrongGuesses.length == 5){
           ShowParts.classList.add("legs")
           showMaxGuesses.textContent = `You have ${maxGuesses-5} guesses left`;
-          LoseText.textContent= `GAME OVER! Rätt ord var "${randomWord}"`;
+          LoseText.textContent= `GAME OVER! Right word was: "${randomWord}".`;
           LoseText.style.color ="white";
           LoseText.style.backgroundColor="red";
           toggleSliderLose();
